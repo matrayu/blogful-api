44d20097c8d5e02460ce31d5f191c2ade8997463
matrayu <northridge614@gmail.com>
2019-05-27 13:12:07 -0700

    Connected server endpoint and tests

--- README.md
+++ README.md
@@ -21,6 +21,10 @@ Start nodemon for the application `npm run dev`
 
 Run the tests `npm test`
 
+## Seeding
+
+psql -U <//user-name> -d <//database> -f ./path/to.seed.sql
+
 ## Deploying
 
 When your new project is ready for deployment, add a new Heroku application with `heroku create`. This will make a new git remote called "heroku" and you can then `npm run deploy` which will push to this remote's master branch.
--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
+const knex = require('knex');
 const app = require('./app')
-const { PORT } = require('./config.js')
+const { PORT, DB_URL } = require('./config.js')
 
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+})
+
+//Using app.set('property-name', 'property-value') we can set 
+//a property called 'db' and set the Knex instance as the value.
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
