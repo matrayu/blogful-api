85f3425b3af114fd48ce9370505cd99a47bc6227
matrayu <northridge614@gmail.com>
2019-05-28 04:15:38 -0700

    Update articles.fixtures.js
    Added maliciousArticle

--- test/articles.fixtures.js
+++ test/articles.fixtures.js
@@ -34,6 +34,28 @@ function makeArticlesArray() {
     ]
 };
 
+function makeMaliciousArticle() {
+    const maliciousArticle = {
+        id: 911,
+        date_published: new Date().toISOString(),
+        style: 'How-to',
+        title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+        content: 'Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.',
+        
+    }
+
+    const expectedArticle = {
+        ...maliciousArticle,
+        title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+        content: 'Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.',
+    }
+    return {
+        maliciousArticle,
+        expectedArticle,
+    }
+}
+
 module.exports = {
     makeArticlesArray,
+    makeMaliciousArticle,
 }
